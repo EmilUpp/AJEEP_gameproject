38ec39b10fb1349bcab39cae1f1ef7d58d9cf667
EmilUpp <emil.sitell@gmail.com>
2020-10-12 17:33:50 +0200

    Created planet script
    Script for handling planets, calculates mass and allows changes in denisty and diameter

---
+++ Our cool gameproject/Assets/planetScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class planetScript : MonoBehaviour
+{
+    public int diameter = 2;
+    public int density = 1;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = new Vector3(diameter, diameter);
+        float volume = 4 * Mathf.PI * Mathf.Pow((diameter / 2f), 3) / 3;
+
+        rb.mass = volume * density;
+    }
+}
