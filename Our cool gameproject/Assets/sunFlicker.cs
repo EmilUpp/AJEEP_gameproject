bf21595f3e209b5a29e1b1551c0a2656c322e780
EmilUpp <emil.sitell@gmail.com>
2020-10-14 21:05:24 +0200

    Added noise to scene

---
+++ Our cool gameproject/Assets/sunFlicker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sunFlicker : MonoBehaviour
+{
+    float value = 0.05f;
+    float timer;
+    private void FixedUpdate()
+    {
+        if (timer % 16 == 0)
+        {
+            gameObject.GetComponent<planetScript>().lacunarity -= value;
+            value *= -1;
+        }
+        timer++;
+    }
+}
