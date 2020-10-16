d3ccacda9612e8f263f733a23cd4a987b71293b0
EmilUpp <emil.sitell@gmail.com>
2020-10-17 01:57:09 +0200

    Commented file

--- Our cool gameproject/Assets/solarSystemGenerator.cs
+++ Our cool gameproject/Assets/solarSystemGenerator.cs
@@ -3,6 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+/*
+ * Class for generating a solar system according to set parameters
+ * 
+ * numberOfPlanets, number of planets in the system
+ * 
+ * minMoonAmount, lowest amount of moons a planet can have
+ * 
+ * maxMoonAmount, maximum amount of moons a planet can have
+ * 
+ * nestSystem, creates a new system
+ */
 public class solarSystemGenerator : MonoBehaviour
 {
     public GameObject planetPrefab;
@@ -27,7 +38,6 @@ public class solarSystemGenerator : MonoBehaviour
         if (newSystem)
         {
             newSystem = false;
-            Debug.Log("New stuff");
             createNewSystem();
         }
 
@@ -35,70 +45,81 @@ public class solarSystemGenerator : MonoBehaviour
 
     void createNewSystem()
     {
-        // Create sun
+        // Creates the system
+
+        // Create sun as base object
         GameObject sun = Instantiate(planetPrefab);
         sun.transform.position = transform.position;
         sun.name = "Sun";
+
+        // Removes orbit from prefab
         orbitAroundBody sunOrbitScript = sun.GetComponent<orbitAroundBody>();
         Destroy(sunOrbitScript);
 
+        // Starts its script
         sun.GetComponent<planetScript>().Start();
-        sun.GetComponent<planetScript>().generatePlanet();
 
+        // Adds planets orbiting itself
         addSubPlanets(sun, numberOfPlanets, new int[] { minMoonAmount, maxMoonAmount}, moonDepth);
 
     }
 
     public void addSubPlanets(GameObject mainBody, int numberOfPlanets, int[] numberOfMoonsRange, int maxMoonDepth)
     {
+        // Adds planets to the sun
+
         for (int i=0; i< numberOfPlanets; i++)
         {
             // Create planet for each in numberOfPlanet
             GameObject newPlanet = Instantiate(planetPrefab, mainBody.transform);
-            newPlanet.name = "Planet: " + i;
+            newPlanet.name = "Planet " + i;
 
+            // Start the orbitAroundBody script
             newPlanet.GetComponent<orbitAroundBody>().Start();
             newPlanet.GetComponent<orbitAroundBody>().speed = Random.Range(200, 800);
 
+            // Start the planetScript
             newPlanet.GetComponent<planetScript>().Start();
-            newPlanet.GetComponent<planetScript>().generatePlanet();
             newPlanet.GetComponent<planetScript>().diameter = mainBody.GetComponent<planetScript>().diameter / 3;
 
-            // set position
-            float parentDistanceToGrandparent = 40; //mainBody.GetComponentInParent<orbitAroundBody>().desiredDistance / 4;
+            // set position, each planet gets expoentially further out
+            float parentDistanceToGrandparent = 40;
             newPlanet.transform.position += new Vector3(parentDistanceToGrandparent * Mathf.Pow(i + 1, 2) * Random.Range(0.75f, 1.25f),
                                                         parentDistanceToGrandparent * Mathf.Pow(i + 1, 2) * Random.Range(0.75f, 1.25f), 0);
 
-            // Recursivly call addMoons
+            // Add moons to the planet
             addMoons(newPlanet, numberOfMoonsRange, 0, maxMoonDepth);
         }
     }
 
     public void addMoons(GameObject mainBody, int[] numberOfMoonsRange, int currentMoonDepth, int maxMoonDepth)
     {
+        // Adds moons to planets, very similiar to addSubPlanets but with somewhat changed values and recusivly adding submoons
+
         int numberOfMoons = Random.Range(numberOfMoonsRange[0], numberOfMoonsRange[1]);
+
         for (int i = 0; i < numberOfMoons; i++)
         {
-            // Create planet for each in numberOfPlanet
             GameObject newPlanet = Instantiate(planetPrefab, mainBody.transform);
             newPlanet.name = GenerateMoonName(mainBody.name, currentMoonDepth);
 
+            // Start the orbitAroundBody script
             newPlanet.GetComponent<orbitAroundBody>().Start();
             newPlanet.GetComponent<orbitAroundBody>().speed = -mainBody.GetComponentInParent<orbitAroundBody>().speed * 1.1f * Random.Range(0.8f, 2f);
 
+            // Start the planetScript
             newPlanet.GetComponent<planetScript>().Start();
-            newPlanet.GetComponent<planetScript>().generatePlanet();
             newPlanet.GetComponent<planetScript>().diameter = mainBody.GetComponent<planetScript>().diameter / 2;
 
             // set position
             float parentDistanceToGrandparent = 5;
-            Debug.Log(Mathf.Sqrt(i / 2) + " " + currentMoonDepth);
             newPlanet.transform.position += new Vector3(parentDistanceToGrandparent * currentMoonDepth * Random.Range(0.75f, 1.25f),
                                                         parentDistanceToGrandparent * currentMoonDepth * Random.Range(0.75f, 1.25f), 0);
 
-            // Recursivly call addMoons
+            // Lower the amount of submoons possible
             int newNumberOfMoons = (int)Mathf.Sqrt(numberOfMoons);
 
+            // Recursivly call addMoons
             if (newNumberOfMoons > 0 && currentMoonDepth < maxMoonDepth)
             {
                 int newMoonDepth = currentMoonDepth + 1;
@@ -109,7 +130,8 @@ public class solarSystemGenerator : MonoBehaviour
 
     static string GenerateMoonName(string mainBodyName, int moonDepth)
     {
-        //string rawName = mainBodyName.Substring(0, "Planet: 0".Length);
+        // Creates a string with appropriate amounts of sub
+
         return string.Concat(Enumerable.Repeat("sub", moonDepth)) + "moon";
     }
 }
