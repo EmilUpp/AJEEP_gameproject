67f3b2d094be46ae91635706d31899e2ad190d6e
EmilUpp <emil.sitell@gmail.com>
2020-10-25 02:14:00 +0100

    Refractored scripts
    Encapsulated all actions to seperate functions to prepare for automatic driving in the future

--- Our cool gameproject/Assets/spaceshipController.cs
+++ Our cool gameproject/Assets/spaceshipController.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 /*
  * Class for handling the spaceships movement
  * 
@@ -16,13 +14,21 @@ using UnityEngine;
  */
 public class spaceshipController : MonoBehaviour
 {
+    [Header("Thrusters")]
     public float rotationalThrust;
     public float trimmThrust;
     public float forwardThrust;
 
+    [Header("Cargo")]
     public float fuelCapacity;
-
     public float fuel;
+
+    public float loadCapacity;
+    public float load;
+
+    public float shipMass;
+
+    private bool angularStabilizerOn;
 
     private Rigidbody2D rb;
 
@@ -31,59 +37,128 @@ public class spaceshipController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         fuel = fuelCapacity;
+
+        angularStabilizerOn = false;
     }
 
     // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            if (angularStabilizerOn)
+            {
+                angularStabilizerOn = false;
+            }
+            else
+            {
+                angularStabilizerOn = true;
+            }
+        }
+    }
+
     void FixedUpdate()
     {
+        rb.mass = shipMass + load;
+
+
+        if (load > loadCapacity)
+        {
+            load = loadCapacity;
+        }
+
+
         // Trimm controls
         if (Input.GetAxis("Vertical") != 0)
         {
-
-            fuel -= (trimmThrust / 2500);
-
-            rb.AddForce(new Vector2(Input.GetAxis("Vertical") * trimmThrust * Mathf.Cos((90 + rb.rotation) * Mathf.Deg2Rad),
-                                    Input.GetAxis("Vertical") * trimmThrust * Mathf.Sin((90 + rb.rotation) * Mathf.Deg2Rad)));
+            FireVerticalTrimEngines();
         }
 
         if (Input.GetAxis("Horizontal") != 0)
         {
-
-            fuel -= (trimmThrust / 2500);
-
-            rb.AddForce(new Vector2(Input.GetAxis("Horizontal") * trimmThrust * Mathf.Cos((rb.rotation) * Mathf.Deg2Rad),
-                                    Input.GetAxis("Horizontal") * trimmThrust * Mathf.Sin((rb.rotation) * Mathf.Deg2Rad)));
+            FireHorizontalTrimEngines();
         }
 
         // Rotation controls
         if (Input.GetKey(KeyCode.Q))
         {
-            float rotation = rotationalThrust;
-            rb.AddTorque(rotation);
-
-            fuel -= (rotationalThrust / 2500);
+            RotateLeft();
         }
         if (Input.GetKey(KeyCode.E))
         {
-            float rotation = rotationalThrust;
-            rb.AddTorque(-rotation);
-
-            fuel -= (rotationalThrust / 2500);
+            RotateRight();
         }
-
 
         // Main engine controls
         if (Input.GetKey(KeyCode.Space) && fuel > 0)
         {
-            rb.AddForce(new Vector2(forwardThrust * Mathf.Cos((90 + rb.rotation) * Mathf.Deg2Rad),
-                                    forwardThrust * Mathf.Sin((90 + rb.rotation) * Mathf.Deg2Rad)));
+            FireForwardThrust();
+        }
 
-            fuel -= (forwardThrust / 1000);
+        if (angularStabilizerOn)
+        {
+            AngularStabilizer();
         }
 
         if (fuel < 0)
         {
             fuel = 0;
+        }
+    }
+
+    void FireVerticalTrimEngines()
+    {
+
+        fuel -= (trimmThrust / 2500);
+
+        rb.AddForce(new Vector2(Input.GetAxis("Vertical") * trimmThrust * Mathf.Cos((90 + rb.rotation) * Mathf.Deg2Rad),
+                                Input.GetAxis("Vertical") * trimmThrust * Mathf.Sin((90 + rb.rotation) * Mathf.Deg2Rad)));
+    }
+
+    void FireHorizontalTrimEngines()
+    {
+        fuel -= (trimmThrust / 2500);
+
+        rb.AddForce(new Vector2(Input.GetAxis("Horizontal") * trimmThrust * Mathf.Cos((rb.rotation) * Mathf.Deg2Rad),
+                                Input.GetAxis("Horizontal") * trimmThrust * Mathf.Sin((rb.rotation) * Mathf.Deg2Rad)));
+    }
+
+    void RotateLeft()
+    {
+        float rotation = rotationalThrust;
+        rb.AddTorque(rotation);
+
+        fuel -= (rotationalThrust / 2500);
+    }
+
+    void RotateRight()
+    {
+        float rotation = rotationalThrust;
+        rb.AddTorque(-rotation);
+
+        fuel -= (rotationalThrust / 2500);
+    }
+
+    void FireForwardThrust()
+    {
+        rb.AddForce(new Vector2(forwardThrust * Mathf.Cos((90 + rb.rotation) * Mathf.Deg2Rad),
+                        forwardThrust * Mathf.Sin((90 + rb.rotation) * Mathf.Deg2Rad)));
+
+        fuel -= (forwardThrust / 1000);
+    }
+
+    void AngularStabilizer()
+    {
+        // Counteracts all angular velocity to help fly straighter
+
+        if (rb.angularVelocity < 0)
+        {
+            RotateLeft();
+        }
+
+        if (rb.angularVelocity > 0)
+        {
+            RotateRight();
         }
     }
 }
