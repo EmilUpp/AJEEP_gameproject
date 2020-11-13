5ed71e0b009f03df380012b0be079c2d7f9ec7f1
SP3sheep <pontus.forsmarks@gmail.com>
2020-11-13 22:35:41 +0100

    simple left/right movement

--- Our cool gameproject/Assets/Scripts/playerMovement.cs
+++ Our cool gameproject/Assets/Scripts/playerMovement.cs
@@ -6,11 +6,14 @@ public class playerMovement : MonoBehaviour
 {
     public float walkSpeed;
     public float jumpForce;
+    public GameObject groundCheck;
+    public LayerMask planetsLayer;
+    private Rigidbody2D rb;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -34,6 +37,11 @@ public class playerMovement : MonoBehaviour
         if(closestPlanet != null)
         {
             transform.rotation = Quaternion.Euler(0, 0, 90 + Mathf.Atan2(closestPlanet.position.y - transform.position.y, closestPlanet.position.x - transform.position.x) * 180 / Mathf.PI);
+        }
+
+        if(Input.GetAxisRaw("Horizontal") != 0 && Physics2D.OverlapCircle(groundCheck.transform.position, 0.1f, planetsLayer))
+        {
+            rb.AddRelativeForce(new Vector2(Input.GetAxisRaw("Horizontal"), 0));
         }
     }
 }
