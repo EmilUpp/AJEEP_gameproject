81a773f87577c5fbd71bac09d3b78794086326ed
SP3sheep <pontus.forsmarks@gmail.com>
2020-11-26 15:57:16 +0100

    Put walking in a fixed update

--- Our cool gameproject/Assets/Scripts/playerMovement.cs
+++ Our cool gameproject/Assets/Scripts/playerMovement.cs
@@ -9,6 +9,7 @@ public class playerMovement : MonoBehaviour
     public GameObject groundCheck;
     public LayerMask planetsLayer;
     private Rigidbody2D rb;
+    private bool canWalk;
     
     // Start is called before the first frame update
     void Start()
@@ -21,16 +22,28 @@ public class playerMovement : MonoBehaviour
     {
         if(Physics2D.OverlapCircle(groundCheck.transform.position, 0.01f, planetsLayer))
         {
-            rb.AddRelativeForce(new Vector2(Input.GetAxisRaw("Horizontal") * walkSpeed, -0.3f));
+            canWalk = true;
 
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 rb.AddRelativeForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             }
         }
+        else
+        {
+            canWalk = false;
+        }
     }
-    
-    
+
+    private void FixedUpdate()
+    {
+        if (canWalk)
+        {
+            rb.AddRelativeForce(new Vector2(Input.GetAxisRaw("Horizontal") * walkSpeed, -0.3f), ForceMode2D.Force);
+        }
+    }
+
+
     private void OnTriggerStay2D(Collider2D collision)
     {
         // If in atmosphere
